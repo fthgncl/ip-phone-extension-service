43fe82441982284c4a965a49681784866a95bfb4
fthgncl <gencal.fatih61@gmail.com>
2021-07-26 16:59:21 +0300

    ...

--- extensionScripts/phoneExtension.js
+++ extensionScripts/phoneExtension.js
@@ -1,124 +1,156 @@
 (function () {
-    //todo:otomatik kullanıcı girişi ekle.
-    let telefon = kayitliTelefonuBul();
-    if (telefon === undefined) {
-        try {
-            telefon = sayfaninHangiTelefonaAitOldBul();
-        } catch (e) {
-            alert(`• ${e.message}`);
-        }
+  //todo:otomatik kullanıcı girişi ekle.
+  //todo: token cookie kayıtını belirli bir zaman sonra otomatik olarak sildir. aksi taktirde o token ile yabancı bir kişi şifre ve password öğrenebilir.
+
+  let telefon = kayitliTelefonuBul();
+  if (telefon === undefined) {
+    try {
+      telefon = sayfaninHangiTelefonaAitOldBul();
+    } catch (e) {
+      alert(`• ${e.message}`);
     }
-    if (telefon !== undefined) {
-        kullaniciGirisIslemleri(telefon);
-    }
+  }
+  if (telefon !== undefined) {
+    kullaniciGirisiniBaslat(telefon);
+  }
 }());
 
-function kullaniciGirisIslemleri(telefon) {
 
+async function kullaniciGirisiniBaslat(telefon) {
+  const token = tokenGetir();
+  if (token) {
+    await token_GirisYap(token);
+  } else {
     let email = prompt('Kullanıcı Adı');
     let pass = prompt('Parola');
+  }
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify({
-        "email": email,
-        "pass": pass,
-        "phone": JSON.stringify(telefon)
+}
+
+function tokenGetir() {
+  return read_cookie("token");
+}
+
+function tokenKaydet() {
+  return save_cookie("token", token);
+}
+
+function token_GirisYap(token) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({token: token})
+  };
+
+  fetch(`${serviceURL}/login`, requestOptions)
+
+
+}
+
+
+/*
+function kullaniciGirisIslemleri(phone) {
+
+  let email = prompt('Kullanıcı Adı');
+  let pass = prompt('Parola');
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    email,
+    pass,
+    phone
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw
+  };
+  fetch(`${serviceURL}/login`, requestOptions)
+    .then(response => {
+
+      if (response.status === 401) {
+        return response.text();
+
+      } else if (response.status !== 200) {
+        throw new Error(`Hata (${response.status}) : Giriş Başarısız.`);
+      }
+
+      return response.json();
+    })
+    .then(result => {
+      if (result) {
+        tokenKaydet(result.token)
+        sayfayaDinamikScriptEkle(result.script)
+      }
+    })
+    .catch(error => {
+      alert(error);
+      console.error('error', error)
     });
 
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw
-    };
-    fetch(serviceURL, requestOptions)
-        .then(response => {
-
-            if (response.status === 401) {
-                return response.text();
-
-            } else if (response.status !== 200) {
-                throw new Error(`Hata (${response.status}) : Giriş Başarısız.`);
-            }
-
-            return response.text();
-        })
-        .then(result => {
-            if (result) {
-                sayfayaDinamikScriptEkle(result)
-            }
-        })
-        .catch(error => {
-            alert(error);
-            console.error('error', error)
-        });
-
 }
+
 
 function sayfayaDinamikScriptEkle(script) {
-    const head = document.getElementsByTagName("head")[0];
-    if (head) {
-        let element = document.createElement("script");
-        element.innerHTML = script;
-        head.appendChild(element);
-    }
+  const head = document.getElementsByTagName("head")[0];
+  if (head) {
+    let element = document.createElement("script");
+    element.innerHTML = script;
+    head.appendChild(element);
+  }
 }
 
-function kayitliTelefonuBul() {
-    const kayitlitelefon = read_cookie("kayitli_telefon");
-    if (kayitlitelefon)
-        return JSON.parse(kayitlitelefon)
+*/
 
-    return undefined;
+
+function read_cookie(kayityeri) {
+
+  let cerez_degeri;
+  const cerezler = document.cookie.split("; ");
+  for (let i = 0; i < cerezler.length; i++) {
+    cerez_degeri = cerezler[i].split("=");
+    if (kayityeri === cerez_degeri[0]) {
+      return cerez_degeri[1];
+    }
+  }
+}
+
+function save_cookie(kayityeri, veri) {
+  document.cookie = (kayityeri + "=" + veri)
 }
 
 function sayfaninHangiTelefonaAitOldBul() {
 
-    let telefon = {};
+  let telefon = {};
 
-    if (document.getElementById("idUsername") !== null && document.title === "Yealink T30 Phone") {      //### SIP-T30 Modeli Algılama
-        telefon.marka = "yealink";
-        telefon.model = "sip-t30";
-    }
-    telefon.marka = "yealink"; //todo:burası kaldırılacak.
+  if (document.getElementById("idUsername") !== null && document.title === "Yealink T30 Phone") {      //### SIP-T30 Modeli Algılama
+    telefon.marka = "yealink";
     telefon.model = "sip-t30";
-    const markaTanimliMi = !telefon.marka;
-    const modelTanimliMi = !telefon.model;
+  }
+  telefon.marka = "yealink"; //todo:burası kaldırılacak.
+  telefon.model = "sip-t30";
+  const markaTanimliMi = !telefon.marka;
+  const modelTanimliMi = !telefon.model;
 
-    if (markaTanimliMi || modelTanimliMi) {
-        throw new Error("Telefon arayüz giriş sayfası tesbit edilemedi.");
-    } else {
-        save_cookie("kayitli_telefon", JSON.stringify(telefon));
-        return telefon;
-    }
+  if (markaTanimliMi || modelTanimliMi) {
+    throw new Error("Telefon arayüz giriş sayfası tesbit edilemedi.");
+  } else {
+    save_cookie("kayitli_telefon", JSON.stringify(telefon));
+    return telefon;
+  }
 }
 
-function read_cookie(kayityeri) {
+function kayitliTelefonuBul() {
+  const kayitlitelefon = read_cookie("kayitli_telefon");
+  if (kayitlitelefon)
+    return JSON.parse(kayitlitelefon)
 
-    let cerez_degeri;
-    const cerezler = document.cookie.split("; ");
-    for (let i = 0; i < cerezler.length; i++) {
-        cerez_degeri = cerezler[i].split("=");
-        if (kayityeri === cerez_degeri[0]) {
-            return cerez_degeri[1];
-        }
-    }
+  return undefined;
 }
-
-function save_cookie(kayityeri, veri) {
-    document.cookie = (kayityeri + "=" + veri)
-}
-
-
-
-
-
-
-
-
-
-
 
 
 
--- service.js
+++ service.js
@@ -2,92 +2,157 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const api_token_key = "api_token_key";
+
 startService();
 
 async function startService() {
 
-    app.use(cors({origin: '*'}));
+  app.use(cors({origin: '*'}));
 
-    app.get("/*", (request, response) => {
-        fs.readFile(`extensionScripts${request.url}`, (error, data) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            response.send(`${data}`);
-        });
+  app.get("/*", (request, response) => {
+    fs.readFile(`extensionScripts${request.url}`, (error, data) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      response.send(`${data}`);
     });
+  });
 
-    app.post("/", async (req, res) => {
-        const kullanici = await uzantidanGonderilenKullaniciBilgileriniCek(req);
 
-        let kullaniciMevcutMu;
-        await kayitliKullaniciyiCek(kullanici.email, kullanici.pass)
-            .then(kullaniciVarligi => kullaniciMevcutMu = kullaniciVarligi);
+  app.post("/login", async (req, res) => {
+    let token, user;
+    await getToken(req) //todo:burada gerçekten await gerekiyor mu ?
+      .then(data => token = data.token)
 
-        const phone = telefon = JSON.parse(kullanici.phone);
+    if (token) {
 
-        if (kullaniciMevcutMu) {
-            logKaydiEkle(`${kullanici.email} kullanıcı adı ile giriş yaptıldı. Tesbit edilen telefon ${phone.marka} ${phone.model}`);
-            let phoneScriptData = await telefonScriptDosyasiniCek(phone);
-            return res.send(phoneScriptData);
-        }
-        logKaydiEkle(`${kullanici.email} kullanıcı adı ile giriş yapılmaya çalışıldı.`, "HATALI GİRİŞ");
-        res.status(401).send('kullanıcı adı veya parola hatalı!');
-    });
+    } else {
+      getUser_emailPass(user)
+        .then(data => user = data)
+        .catch(error => console.error(error))
+    }
+
+  });
 }
 
-function logKaydiEkle(bilgi, logBasligi = '') {
-    let tarih = new Date();
-    (logBasligi !== '' ) && (logBasligi = `[${logBasligi}]`);
-    let tarihYazisi = `[Tarih : ${tarih.getDate()}/${tarih.getMonth() + 1}/${tarih.getFullYear()} - ${tarih.getHours()}:${tarih.getMinutes()}]`
-    fs.appendFile("logs.txt", `\n${tarihYazisi} ${logBasligi}\n${bilgi}\n`, err => {
-        if (err) {
-            throw err;
-        }
+function getUser_emailPass() {
+  let email = prompt('Kullanıcı Adı');
+  let pass = prompt('Parola');
+
+  return new Promise((result, reject) => {
+    fs.readFile("users.json", (error, data) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      const kullanicilar = JSON.parse(data.toString());
+      const kullanci = kullanicilar.find(kullanici => (kullanici.email === useremail && kullanici.pass === userpass))
+      if (kullanci) {
+        result(kullanci);
+      } else {
+        reject("Kullanıcıadı veya parola hatalı.");
+      }
     });
+  });
+}
+
+function getToken(request) {
+  return new Promise((resolve) => {
+    request.on('data', chunk => {
+      if (chunk) {
+        resolve(JSON.parse(chunk.toString()));
+      } else {
+        resolve(false)
+      }
+    });
+  }).catch(error => console.error(error));
+}
+
+app.listen(3000);
+
+
+/*
+function tokenUret(userID, gecerlilikSuresi = "300s") {
+  return jwt.sign(userID, 'api_token_key', {expiresIn: gecerlilikSuresi});
+}
+
+
+function logKaydiEkle(bilgi, logBasligi = '') {
+  let tarih = new Date();
+  (logBasligi !== '') && (logBasligi = `[${logBasligi}]`);
+  let tarihYazisi = `[Tarih : ${tarih.getDate()}/${tarih.getMonth() + 1}/${tarih.getFullYear()} - ${tarih.getHours()}:${tarih.getMinutes()}]`
+  fs.appendFile("logs.txt", `\n${tarihYazisi} ${logBasligi}\n${bilgi}\n`, err => {
+    if (err) {
+      throw err;
+    }
+  });
 
 
 }
 
 function telefonScriptDosyasiniCek(telefon) {
 
-    return new Promise(result => {
-        fs.readFile(`extensionScripts/phoneScripts/${telefon.marka}-${telefon.model}.js`, (error, data) => {
-            if (error) {
-                console.error(error);
-            }
-            result(data);
-        })
-    });
+  return new Promise(result => {
+    fs.readFile(`extensionScripts/phoneScripts/${telefon.marka}-${telefon.model}.js`, (error, data) => {
+      if (error) {
+        console.error(error);
+      }
+      result(data);
+    })
+  });
 
 }
 
 function kayitliKullaniciyiCek(useremail, userpass) {
 
-    return new Promise((result) => {
-        fs.readFile("users.json", (error, data) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            const kullanicilar = JSON.parse(data.toString());
-            const kullaniciMevcutMu = kullanicilar.find(kullanici => (kullanici.email === useremail && kullanici.pass === userpass)) !== undefined;
-            result(kullaniciMevcutMu);
-        });
+  return new Promise((result) => {
+    fs.readFile("users.json", (error, data) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      const kullanicilar = JSON.parse(data.toString());
+      const kullaniciMevcutMu = kullanicilar.find(kullanici => (kullanici.email === useremail && kullanici.pass === userpass)) !== undefined;
+      result(kullaniciMevcutMu);
     });
+  });
 
 }
 
 function uzantidanGonderilenKullaniciBilgileriniCek(request) {
-    return new Promise((resolve) => {
-        request.on('data', chunk => {
-            resolve(JSON.parse(chunk.toString()));
-        });
-    }).catch(err => console.log(err));
-}
 
-app.listen(3000);
+  return new Promise((resolve) => {
+    request.on('data', chunk => {
+      const user = JSON.parse(chunk.toString());
+
+      resolve(user);
+    });
+  }).catch(err => console.log(err));
+}
+*/
+/*  app.post("/login", async (req, res) => {
+    const kullanici = await uzantidanGonderilenKullaniciBilgileriniCek(req);
+
+
+    let kullaniciMevcutMu;
+    await kayitliKullaniciyiCek(kullanici.email, kullanici.pass)
+      .then(kullaniciVarligi => kullaniciMevcutMu = kullaniciVarligi);
+
+    const phone = telefon = JSON.parse(kullanici.phone);
+
+    if (kullaniciMevcutMu) {
+      token = await tokenUret(kullanici);
+      logKaydiEkle(`${kullanici.email} kullanıcı adı ile giriş yaptıldı. Tesbit edilen telefon ${phone.marka} ${phone.model}`);
+      let phoneScriptData = await telefonScriptDosyasiniCek(phone);
+      return res.send({token, script: phoneScriptData});
+    }
+    logKaydiEkle(`${kullanici.email} kullanıcı adı ile giriş yapılmaya çalışıldı.`, "HATALI GİRİŞ");
+    res.status(401).send('kullanıcı adı veya parola hatalı!');
+  });*/
+
 
 
 
