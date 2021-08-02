399bf1c572b3d45dc47ba1aebe4c6972f9f6b29f
fthgncl <gencal.fatih61@gmail.com>
2021-08-02 13:41:50 +0300

    02/08/2021

--- extensionScripts/stopinstall.js
+++ extensionScripts/stopinstall.js
@@ -1,15 +1,17 @@
-save_cookie("startinstall","0") // Birtek bu kayıt uzantı üzerinde tanımlı.
-save_cookie("asama","0")
+// Uzantının simgesine sağtıklayıp "Kurulum İşlemlerini Durdur" butonuna bastığında
+// ve kurulum işlemi tamamlandığında çalışır.
+ 
+save_cookie("startinstall", "0")
+save_cookie("asama", "0")
 delete_cookie("kayitli_telefon")
 alert("• Kurulum İşlemleri Durduruldu.")
 window.location.href = '/'
 
-function delete_cookie(kayityeri){
-    document.cookie = kayityeri+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-function save_cookie(kayityeri,veri){
-    document.cookie = (kayityeri+"="+veri)
+function delete_cookie(kayityeri) {
+  document.cookie = kayityeri + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
-//todo: otomatik kullanıcı girişini bu sayfada kapat.
+function save_cookie(kayityeri, veri) {
+  document.cookie = (kayityeri + "=" + veri)
+}
 
